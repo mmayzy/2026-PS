3c21c60899d459e315ec7cb693e22779d6acd06f
mmayzy <20251ctb0100030@estudantes.ifpr.edu.br>
2026-03-12 15:24:24 +0000

    aula 7

--- 2026-PS/00_revisaoPython/05_modulos/utils/__init__.py
+++ 2026-PS/00_revisaoPython/05_modulos/utils/__init__.py
@@ -1,3 +1,5 @@
 # utils/__init__.py
-
+from .validador import validar_numero
 from .formatador import linha_separadora, formatar_resultado, cabecalho_secao
+
+__all__ = ['validar_numero', 'cabecalho_secao', 'formatar_resultado', 'linha_separadora']
