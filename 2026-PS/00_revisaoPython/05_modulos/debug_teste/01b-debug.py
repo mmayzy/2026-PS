716acf1a5d7d91bc5085ea5c624367e019e531df
mmayzy <20251ctb0100030@estudantes.ifpr.edu.br>
2026-03-15 23:01:55 +0000

    debug corrigido

--- 2026-PS/00_revisaoPython/05_modulos/debug_teste/01b-debug.py
+++ 2026-PS/00_revisaoPython/05_modulos/debug_teste/01b-debug.py
@@ -2,7 +2,7 @@
 # ATENÇÃO: 4 erros propositais. Encontre e corrija todos!
 # Rode de dentro de 05_modulos/: python debug_teste/01b-debug.py
 
-from conversores import temperatura 
+''' from conversores import temperatura 
 
 from conversores import celsius_para_kelvin, km_para_milhas
 resultado = celsius_para_kelvin(25)
@@ -10,6 +10,23 @@ print(f"25°C em K: {resultado}")
 
 from utils import formatar_resultado
 print(formatar_resultado("teste", 100, "km", 62.1, "mi"))
+print(f"50 km = {km_para_milhas(50):.2f} mi")'''
+
+import sys # sys é usado para manipular o ambiente
+import os # os é o sistema operacional para acessar os arquivos
+
+sys.path.append(os.path.dirname(os.path.dirname(__file__))) #pega o local do arquivo, sobe de pasta (até a raiz) e mostra ao codigio onde buscar módulos.
+
+from conversores.temperatura import celsius_para_kelvin
+
+#from conversores import celsius_para_kelvin
+resultado = celsius_para_kelvin(25)
+print(f"25°C em K: {resultado}")
+
+from utils.formatador import formatar_resultado
+print(formatar_resultado("teste", 100, "km", 62.1, "mi"))
+
+from conversores import km_para_milhas
 print(f"50 km = {km_para_milhas(50):.2f} mi")
 
 # import algo apagado
