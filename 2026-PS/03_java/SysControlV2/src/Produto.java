4ff31892878fb72492a0b730f789d8b962a27215
mmayzy <20251ctb0100030@estudantes.ifpr.edu.br>
2026-07-28 14:49:58 +0000

    Adicionados atributos privados e construtor

--- 2026-PS/03_java/SysControlV2/src/Produto.java
+++ 2026-PS/03_java/SysControlV2/src/Produto.java
@@ -1,3 +1,17 @@
 public class Produto {
 
+    // Atributos
+    private int codigo;
+    private String nome;
+    private double preco;
+    private int quantidade;
+
+    // Construtor
+    public Produto(int codigo, String nome, double preco, int quantidade) {
+        this.codigo = codigo;
+        this.nome = nome;
+        this.preco = preco;
+        this.quantidade = quantidade;
+    }
+
 }
